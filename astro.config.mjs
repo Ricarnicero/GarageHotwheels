4fef52a4a80a33f52c569bc11d95a176b7370de6
Ricarnicero <ricardots211@gmail.com>
2025-09-12 23:57:10 -0600

    Update Astro build command and adjust base path for GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/GarageHotwheels' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/GarageHotwheels' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
